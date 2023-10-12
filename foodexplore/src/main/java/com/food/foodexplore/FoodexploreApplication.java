bb3b68b33db3865a3660d984bf4d416ed175c682
GitDongHun <112303dh@naver.com>
2023-10-12 14:19:36 +0900

    첫 프로젝트 커밋

---
+++ foodexplore/src/main/java/com/food/foodexplore/FoodexploreApplication.java
@@ -0,0 +1,13 @@
+package com.food.foodexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodexploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodexploreApplication.class, args);
+	}
+
+}
