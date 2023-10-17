db24b03336b5ddbc74bdfd9b3bf07e13c03b230d
JuAnLee <dlwndks9444@naver.com>
2023-10-17 17:33:56 +0900

    MainController, main.html 수정

--- foodexplore/src/main/java/com/food/exp/controller/MainController.java
+++ foodexplore/src/main/java/com/food/exp/controller/MainController.java
@@ -1,13 +1,14 @@
 package com.food.exp.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
-@SpringBootApplication
+@Controller
 public class MainController {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainController.class, args);
+		
+
+		
 
 	}
 
--- foodexplore/src/main/java/com/food/exp/dto/MainDTO.java
+++ foodexplore/src/main/java/com/food/exp/dto/MainDTO.java
@@ -2,4 +2,6 @@ package com.food.exp.dto;
 
 public class MainDTO {
 
+	
+	
 }
