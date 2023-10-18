0c8b172069ff27bb98bb9d444b8a72a0deaff079
JuAnLee <dlwndks9444@naver.com>
2023-10-18 10:23:22 +0900

    백업

--- foodexplore/src/main/java/com/food/exp/controller/MainController.java
+++ foodexplore/src/main/java/com/food/exp/controller/MainController.java
@@ -1,15 +1,16 @@
 package com.food.exp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-	public static void main(String[] args) {
-		
+		@GetMapping("main")
+		public String main() {
+			return "MainController";
+		}
 
-		
 
-	}
 
 }
