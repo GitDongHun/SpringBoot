aba387fbf8c58be641f6dda59e9a6b9677ad83e0
hyunbinheaven <shb7689@naver.com>
2023-10-18 14:41:02 +0900

    리뷰페이지 구상

--- foodexplore/src/main/java/com/food/exp/controller/MainController.java
+++ foodexplore/src/main/java/com/food/exp/controller/MainController.java
@@ -1,14 +1,25 @@
 package com.food.exp.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-		@GetMapping("main")
+	@Autowired
+	ServletContext application;
+	
+		@GetMapping("/main")
 		public String main() {
 			return "main";
+		}
+		
+		@GetMapping("/rev")
+		public String rev() {
+			return "/rev/rev";
 		}
 
 }
