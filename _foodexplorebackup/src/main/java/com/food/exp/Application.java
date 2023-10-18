e0042d375aa4e4f7238d5d0c6bf4940d646d991e
GitDongHun <112303dh@naver.com>
2023-10-18 10:40:41 +0900

    foodexplore 프로젝트 재생성  _foodexplorerbackup

--- _foodexplorebackup/src/main/java/com/food/exp/Application.java
+++ _foodexplorebackup/src/main/java/com/food/exp/Application.java
@@ -3,11 +3,15 @@ package com.food.exp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.food.exp.controller.MainController;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(MainController.class, args);
+		System.out.println("FoodExploreMainController 실행");
+
 	}
 
 }
