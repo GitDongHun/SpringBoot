5d1eb9143f7e4137ab309597e8538ca4e7225785
SeohyunPark <ttol6354@gmail.com>
2023-10-19 14:08:26 +0900

    헤더 css 수정

--- foodexplore/src/main/java/com/food/exp/controller/MemberController.java
+++ foodexplore/src/main/java/com/food/exp/controller/MemberController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MemberController {
 
 	@GetMapping("/joinform")
-	public String main() {
+	public String join() {
 		System.out.println("MemberController");
 		return "member/joinform";
+	}
+
+	@GetMapping("/loginform")
+	public String loginform() {
+		System.out.println("MemberController");
+		return "member/loginform";
 	}
 }
