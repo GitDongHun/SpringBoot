9cb36adf73035589346b98f7d7acc998bf3eb73c
SeohyunPark <ttol6354@gmail.com>
2023-10-19 14:43:55 +0900

    member mapper 생성

--- foodexplore/src/main/java/com/food/exp/controller/MemberController.java
+++ foodexplore/src/main/java/com/food/exp/controller/MemberController.java
@@ -2,6 +2,9 @@ package com.food.exp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.food.exp.dto.MemberDTO;
 
 @Controller
 public class MemberController {
@@ -10,6 +13,13 @@ public class MemberController {
 	public String join() {
 		System.out.println("MemberController");
 		return "member/joinform";
+	}
+	
+	// 2. 글쓰기
+	@PostMapping(value = "/join")
+	public String write(MemberDTO dto) {
+		//int num = service.noticeWrite(notice);
+		return "redirect:main";
 	}
 
 	@GetMapping("/loginform")
