321b438f10662b872d29949e379ec1d485d9c0e0
JuAnLee <dlwndks9444@naver.com>
2023-11-08 18:09:49 +0900

    top10 이미지, 링크 연결, UI 수정

--- foodexplore/src/main/java/com/food/exp/controller/MainController.java
+++ foodexplore/src/main/java/com/food/exp/controller/MainController.java
@@ -3,14 +3,18 @@ package com.food.exp.controller;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.food.exp.dto.FileDTO;
 import com.food.exp.dto.MainDTO;
 import com.food.exp.service.MainService;
+import com.food.exp.service.UploadService;
 
 @Controller
 public class MainController {
@@ -20,18 +24,38 @@ public class MainController {
 	
 	@Autowired
 	private MainService service;
+	
+	@Autowired
+	UploadService uploadService;
 
 	
 	@GetMapping("/main")
-	public String main(Model model) {
+	public String main(Model model, HttpSession session) {
 		System.out.println("MainController 실행");
 			
 		List<MainDTO> top10Rst = service.top10Rst();
 		model.addAttribute("top10Rst", top10Rst);
+		
+		// 이미지 가져오기
+	    String rst_id = (String) session.getAttribute("rst_id");
+	    List<FileDTO> attachList = uploadService.getFilesRst(rst_id);
+	    model.addAttribute("attachList", attachList);
 		    
 		return "main";
 	}
-		
+	
+	@RestController
+	public class RestaurantController {
+	    @Autowired
+	    private MainService mainService;
+
+	    @GetMapping("/api/top10Restaurants")
+	    public List<MainDTO> getTop10Restaurants() {
+	        return mainService.top10Rst();
+	    }
+	}
+
+	
 	@GetMapping("/template")
 	public String template_thymeleaf(){
 		return "sample_template/template_thymeleaf";
